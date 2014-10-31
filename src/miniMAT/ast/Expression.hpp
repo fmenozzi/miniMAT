6d850f3deb7124e37529a863e8eeeb5fa1a8213e
Federico Menozzi <federicogmenozzi@gmail.com>
2014-10-31 17:16:46 -0400

    Add Expression node

---
+++ src/miniMAT/ast/Expression.hpp
@@ -0,0 +1,14 @@
+#ifndef EXPRESSION_HPP
+#define EXPRESSION_HPP
+
+#include <AST.hpp>
+
+namespace miniMAT {
+    namespace ast {
+        class Expression : public AST {
+
+        };
+    }
+}
+
+#endif
