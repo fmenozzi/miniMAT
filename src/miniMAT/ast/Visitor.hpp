b3d200019c961b824599f56a35d3aa3038d1ac06
Federico Menozzi <federicogmenozzi@gmail.com>
2014-10-31 18:02:45 -0400

    Get rid of smart pointers (for now)

--- src/miniMAT/ast/Visitor.hpp
+++ src/miniMAT/ast/Visitor.hpp
@@ -1,8 +1,6 @@
 #ifndef VISITOR_HPP
 #define VISITOR_HPP
 
-#include <memory>
-
 #include <ScalarType.hpp>
 
 #include <ExprStmt.hpp>
@@ -16,36 +14,27 @@
 
 namespace miniMAT {
     namespace ast {
-        using std::unique_ptr;
-
         template<typename ArgType, typename ResultType>
         class Visitor {
         public:
             virtual ~Visitor() {}
 
             // Types
-            virtual ResultType VisitScalarType(const unique_ptr<ScalarType>& st,
-                                        ArgType arg) = 0;
+            virtual ResultType VisitScalarType(const ScalarType& st, ArgType arg) = 0;
 
             // Statements
-            virtual ResultType VisitExprStmt(const unique_ptr<ExprStmt>& es,
-                                        ArgType arg) = 0;
+            virtual ResultType VisitExprStmt(const ExprStmt& es, ArgType arg) = 0;
 
             // Expressions
-            virtual ResultType VisitUnaryExpr(const unique_ptr<UnaryExpr>& ue,
-                                        ArgType arg) = 0;
-            virtual ResultType VisitBinaryExpr(const unique_ptr<BinaryExpr>& be,
-                                        ArgType arg) = 0;
-            virtual ResultType VisitLiteralExpr(const unique_ptr<LiteralExpr>& le,
-                                        ArgType arg) = 0;
+            virtual ResultType VisitUnaryExpr(const UnaryExpr& ue, ArgType arg) = 0;
+            virtual ResultType VisitBinaryExpr(const BinaryExpr& be, ArgType arg) = 0;
+            virtual ResultType VisitLiteralExpr(const LiteralExpr& le, ArgType arg) = 0;
 
             // References
 
             // Terminals
-            virtual ResultType VisitFloatLiteral(const unique_ptr<FloatLiteral>& fl,
-                                        ArgType arg) = 0;
-            virtual ResultType VisitOperator(const unique_ptr<Operator>& op,
-                                        ArgType arg) = 0;
+            virtual ResultType VisitFloatLiteral(const FloatLiteral& fl, ArgType arg) = 0;
+            virtual ResultType VisitOperator(const Operator& op, ArgType arg) = 0;
         };
     }
 }
