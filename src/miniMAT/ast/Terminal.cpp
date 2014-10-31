5a5feedb3d6a9965f6364630b1e4712108fb320b
Federico Menozzi <federicogmenozzi@gmail.com>
2014-10-31 17:15:24 -0400

    Add Terminal node

---
+++ src/miniMAT/ast/Terminal.cpp
@@ -0,0 +1,9 @@
+#include <Terminal.hpp>
+
+namespace miniMAT {
+    namespace ast {
+        Terminal::Terminal(const std::string& spelling) {
+            this->spelling = spelling;
+        }
+    }
+}
---
+++ src/miniMAT/ast/Terminal.hpp
@@ -0,0 +1,21 @@
+#ifndef TERMINAL_HPP
+#define TERMINAL_HPP
+
+#include <string>
+
+#include <AST.hpp>
+
+namespace miniMAT {
+    namespace ast {
+        class Terminal : public AST {
+        public:
+            Terminal(const std::string& spelling);
+            virtual ~Terminal() {}
+
+        private:
+            std::string spelling;
+        };
+    }
+}
+
+#endif
