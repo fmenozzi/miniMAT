08a6dad88ead3018d6d1f08443b199a224b2b845
Federico Menozzi <federicogmenozzi@gmail.com>
2014-10-31 10:29:20 -0400

    Add Statement node

---
+++ src/miniMAT/ast/Statement.hpp
@@ -0,0 +1,13 @@
+#ifndef STATEMENT_HPP
+#define STATEMENT_HPP
+
+namespace miniMAT {
+    namespace ast {
+        template<typename ArgType, typename ResultType>
+        class Statement {
+            
+        };
+    }
+}
+
+#endif
