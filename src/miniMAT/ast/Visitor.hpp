8ff7ba53bb8a3de0bf3df0c82b2a746a56885b55
Federico Menozzi <federicogmenozzi@gmail.com>
2014-10-31 09:37:58 -0400

    Add Visitor interface

---
+++ src/miniMAT/ast/Visitor.hpp
@@ -0,0 +1,42 @@
+#ifndef VISITOR_HPP
+#define VISITOR_HPP
+
+#include <memory>
+
+namespace miniMAT {
+    namespace ast {
+        using std::unique_ptr;
+        
+        template<typename ArgType, typename ResultType>
+        class Visitor {
+        public:
+            virtual ~Visitor() {}
+
+            // Types
+            ResultType VisitScalarType(const unique_ptr<ScalarType>& st,
+                                        ArgType arg) = 0;
+
+            // Statements
+            ResultType VisitExprStmt(const unique_ptr<ExprStmt>& es,
+                                        ArgType arg) = 0;
+
+            // Expressions
+            ResultType VisitUnaryExpr(const unique_ptr<UnaryExpr>& ue,
+                                        ArgType arg) = 0;
+            ResultType VisitBinaryExpr(const unique_ptr<BinaryExpr>& be,
+                                        ArgType arg) = 0;
+            ResultType VisitLiteralExpr(const unique_ptr<LiteralExpr>& le,
+                                        ArgType arg) = 0;
+
+            // References
+
+            // Terminals
+            ResultType VisitFloatLiteral(const unique_ptr<FloatLiteral>& fl,
+                                        ArgType arg) = 0;
+            ResultType VisitOperator(const unique_ptr<Operator>& op,
+                                        ArgType arg) = 0;
+        };
+    }
+}
+
+#endif
