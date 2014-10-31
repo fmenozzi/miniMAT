794ba067e36c74490d070f70d71a890c0edc17e3
Federico Menozzi <federicogmenozzi@gmail.com>
2014-10-31 10:09:22 -0400

    Add virtual keyword to methods

--- src/miniMAT/ast/Visitor.hpp
+++ src/miniMAT/ast/Visitor.hpp
@@ -24,27 +24,27 @@ namespace miniMAT {
             virtual ~Visitor() {}
 
             // Types
-            ResultType VisitScalarType(const unique_ptr<ScalarType>& st,
+            virtual ResultType VisitScalarType(const unique_ptr<ScalarType>& st,
                                         ArgType arg) = 0;
 
             // Statements
-            ResultType VisitExprStmt(const unique_ptr<ExprStmt>& es,
+            virtual ResultType VisitExprStmt(const unique_ptr<ExprStmt>& es,
                                         ArgType arg) = 0;
 
             // Expressions
-            ResultType VisitUnaryExpr(const unique_ptr<UnaryExpr>& ue,
+            virtual ResultType VisitUnaryExpr(const unique_ptr<UnaryExpr>& ue,
                                         ArgType arg) = 0;
-            ResultType VisitBinaryExpr(const unique_ptr<BinaryExpr>& be,
+            virtual ResultType VisitBinaryExpr(const unique_ptr<BinaryExpr>& be,
                                         ArgType arg) = 0;
-            ResultType VisitLiteralExpr(const unique_ptr<LiteralExpr>& le,
+            virtual ResultType VisitLiteralExpr(const unique_ptr<LiteralExpr>& le,
                                         ArgType arg) = 0;
 
             // References
 
             // Terminals
-            ResultType VisitFloatLiteral(const unique_ptr<FloatLiteral>& fl,
+            virtual ResultType VisitFloatLiteral(const unique_ptr<FloatLiteral>& fl,
                                         ArgType arg) = 0;
-            ResultType VisitOperator(const unique_ptr<Operator>& op,
+            virtual ResultType VisitOperator(const unique_ptr<Operator>& op,
                                         ArgType arg) = 0;
         };
     }
