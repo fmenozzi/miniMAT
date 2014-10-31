5d778eab11b10d2229ef0b176bb5573f00863348
Federico Menozzi <federicogmenozzi@gmail.com>
2014-10-31 17:26:42 -0400

    Add Literal node

---
+++ src/miniMAT/ast/Literal.hpp
@@ -0,0 +1,18 @@
+#ifndef LITERAL_HPP
+#define LITERAL_HPP
+
+#include <string>
+
+#include <Terminal.hpp>
+
+namespace miniMAT {
+    namespace ast {
+        class Literal : public Terminal {
+        public:
+            Literal(const std::string& spelling) : Terminal(spelling) {}
+            virtual ~Literal() {}
+        };
+    }
+}
+
+#endif
