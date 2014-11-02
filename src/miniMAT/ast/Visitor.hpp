59ec2bdd6f188e47279f164787f814363c09cb98
Federico Menozzi <federicogmenozzi@gmail.com>
2014-11-02 16:12:18 -0500

    Correctly forward-declare scoped classes

--- src/miniMAT/ast/Visitor.hpp
+++ src/miniMAT/ast/Visitor.hpp
@@ -1,39 +1,39 @@
 #ifndef VISITOR_HPP
 #define VISITOR_HPP
 
-class ScalarType;
-
-class ExprStmt;
-
-class UnaryExpr;
-class BinaryExpr;
-class LiteralExpr;
-
-class FloatLiteral;
-class Operator;
-
 namespace miniMAT {
     namespace ast {
+        class ScalarType;
+
+        class ExprStmt;
+
+        class UnaryExpr;
+        class BinaryExpr;
+        class LiteralExpr;
+
+        class FloatLiteral;
+        class Operator;
+
         class Visitor {
         public:
             virtual ~Visitor() {}
 
             // Types
-            virtual void VisitScalarType(const ScalarType& st) = 0;
+            virtual void VisitScalarType(ScalarType& st) = 0;
 
             // Statements
-            virtual void VisitExprStmt(const ExprStmt& es) = 0;
+            virtual void VisitExprStmt(ExprStmt& es) = 0;
 
             // Expressions
-            virtual void VisitUnaryExpr(const UnaryExpr& ue)     = 0;
-            virtual void VisitBinaryExpr(const BinaryExpr& be)   = 0;
-            virtual void VisitLiteralExpr(const LiteralExpr& le) = 0;
+            virtual void VisitUnaryExpr(UnaryExpr& ue)     = 0;
+            virtual void VisitBinaryExpr(BinaryExpr& be)   = 0;
+            virtual void VisitLiteralExpr(LiteralExpr& le) = 0;
 
             // References
 
             // Terminals
-            virtual void VisitFloatLiteral(const FloatLiteral& fl) = 0;
-            virtual void VisitOperator(const Operator& op)         = 0;
+            virtual void VisitFloatLiteral(FloatLiteral& fl) = 0;
+            virtual void VisitOperator(Operator& op)         = 0;
         };
     }
 }
