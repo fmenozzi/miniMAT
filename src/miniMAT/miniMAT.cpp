9292002d911725ec03d9bbdec3c9161db1573a47
Federico Menozzi <federicogmenozzi@gmail.com>
2014-11-12 09:29:50 -0500

    Clean up who/whos

--- src/miniMAT/miniMAT.cpp
+++ src/miniMAT/miniMAT.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <iomanip>
 #include <string>
 #include <memory>
 #include <map>
@@ -35,15 +36,19 @@ int main() {
         } else if (input_line == "") {
             continue;
         } else if (input_line == "who") {
-            // TODO: I'll format this nicer later
             for (auto var : *id_table)
                 std::cout << var.first << std::endl;
-            std::cout << std::endl;
+            std::cout  << std::endl;
             continue;
         } else if (input_line == "whos") {
-            // TODO: I'll format this nicer later
+            // Find longest var name (for formatting)
+            std::size_t max_var_length = 0;
             for (auto var : *id_table)
-                std::cout << var.first << " = " << var.second << std::endl;
+                if (var.first.size() > max_var_length)
+                    max_var_length = var.first.size();
+
+            for (auto var : *id_table)
+                std::cout << std::setw(max_var_length) << var.first << " = " << var.second << std::endl;
             std::cout << std::endl;
             continue;
         }
@@ -68,30 +73,35 @@ int main() {
             std::cout << std::endl;
         } else {
             double ans = ast->VisitEvaluate(id_table);
-            if (!parser.SuppressedOutput()) {
-                if (ast->GetClassName() == "ExprStmt") {
-                    auto exprstmt = std::dynamic_pointer_cast<miniMAT::ast::ExprStmt>(ast);
-                    if (exprstmt->expr->GetClassName() == "RefExpr") {
-                        auto refexpr = std::dynamic_pointer_cast<miniMAT::ast::RefExpr>(exprstmt->expr);
-                        auto varname = std::dynamic_pointer_cast<miniMAT::ast::IdRef>(refexpr->ref)->id->GetSpelling();
+            if (ast->GetClassName() == "ExprStmt") {
+                auto exprstmt = std::dynamic_pointer_cast<miniMAT::ast::ExprStmt>(ast);
+                if (exprstmt->expr->GetClassName() == "RefExpr") {
+                    auto refexpr = std::dynamic_pointer_cast<miniMAT::ast::RefExpr>(exprstmt->expr);
+                    auto varname = std::dynamic_pointer_cast<miniMAT::ast::IdRef>(refexpr->ref)->id->GetSpelling();
 
+                    if (!parser.SuppressedOutput()) {
                         std::cout << varname << " =" << std::endl << std::endl;
                         std::cout << "    " << id_table->at(varname) << std::endl << std::endl;
-                    } else {
-                        (*id_table)["ans"] = ans;
+                    }
+                } else {
+                    (*id_table)["ans"] = ans;
+
+                    if (!parser.SuppressedOutput()) {
                         std::cout << "ans =" << std::endl << std::endl;
                         std::cout << "     " << ans << std::endl << std::endl;
                     }
+                }
 
-                } else if (ast->GetClassName() == "AssignStmt") {
-                    auto assign_stmt = std::dynamic_pointer_cast<miniMAT::ast::AssignStmt>(ast);
+            } else if (ast->GetClassName() == "AssignStmt") {
+                auto assign_stmt = std::dynamic_pointer_cast<miniMAT::ast::AssignStmt>(ast);
 
-                    if (assign_stmt->ref->GetClassName() == "IdRef") {
-                        auto idref   = std::dynamic_pointer_cast<miniMAT::ast::IdRef>(assign_stmt->ref);
+                if (assign_stmt->ref->GetClassName() == "IdRef") {
+                    auto idref   = std::dynamic_pointer_cast<miniMAT::ast::IdRef>(assign_stmt->ref);
 
-                        auto varname = idref->id->GetSpelling();
-                        auto val     = assign_stmt->val;
+                    auto varname = idref->id->GetSpelling();
+                    auto val     = assign_stmt->val;
 
+                    if (!parser.SuppressedOutput()) {
                         std::cout << varname << " =" << std::endl << std::endl;
                         std::cout << "     " << val << std::endl << std::endl;
                     }
