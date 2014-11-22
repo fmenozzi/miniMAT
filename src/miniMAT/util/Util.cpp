2e4272160683ce5f4c4571f42e627720b791f0e9
Federico Menozzi <federicogmenozzi@gmail.com>
2014-11-22 14:30:05 -0500

    Formatting

--- src/miniMAT/util/Util.cpp
+++ src/miniMAT/util/Util.cpp
@@ -5,52 +5,52 @@
 #include <cstdlib>
 
 namespace miniMAT {
-	namespace util {
+    namespace util {
 
-		// auto doesn't seem to work here, for some reason
-		std::map<std::string, std::function<void(void)>> Util::funcs = std::map<std::string, std::function<void(void)>>();
+        // auto doesn't seem to work here, for some reason
+        std::map<std::string, std::function<void(void)>> Util::funcs = std::map<std::string, std::function<void(void)>>();
 
-		void Util::init(std::shared_ptr<std::map<std::string, Matrix>> vars) {
+        void Util::init(std::shared_ptr<std::map<std::string, Matrix>> vars) {
             funcs["quit"] = funcs["exit"] = funcs["bye!"] = []() {
                 throw 1;
             };
 
-			funcs["who"] = [vars]() {
-				using namespace std;
+            funcs["who"] = [vars]() {
+            	using namespace std;
 
-				for (auto var : *vars)
-                	cout << var.first << endl;
-            	cout  << endl;
-			};
+            	for (auto var : *vars)
+                    cout << var.first << endl;
+                cout  << endl;
+            };
 
-			funcs["whos"] = [vars]() {
-				using namespace std;
+            funcs["whos"] = [vars]() {
+                using namespace std;
 
-				auto w = max_element(begin(*vars), end(*vars), [](pair<string, Matrix> p1, pair<string, Matrix> p2) {
-	                return p1.first.size() < p2.first.size();
-	            })->first.size();
+                auto w = max_element(begin(*vars), end(*vars), [](pair<string, Matrix> p1, pair<string, Matrix> p2) {
+                    return p1.first.size() < p2.first.size();
+                })->first.size();
 
-	            cout << endl;
-	            for (auto var : *vars)
-	                cout << setw(w) << var.first << " = " << var.second << endl;
-	            cout << endl;
-			};
+                cout << endl;
+                for (auto var : *vars)
+                    cout << setw(w) << var.first << " = " << var.second << endl;
+                cout << endl;
+            };
 
-			funcs[""] = []() {
+            funcs[""] = []() {
                 return;
-			};
-		}
+            };
+        }
 
-		void Util::PrintResult(std::string varname, Matrix val, bool suppressed) {
-		    if (!suppressed) {
-		        std::cout << varname << " =" << std::endl << std::endl;
-		        std::cout << "    " << val << std::endl << std::endl;
-		    }
-		}
+        void Util::PrintResult(std::string varname, Matrix val, bool suppressed) {
+            if (!suppressed) {
+                std::cout << varname << " =" << std::endl << std::endl;
+                std::cout << "    " << val << std::endl << std::endl;
+            }
+        }
 
-		std::function<void(void)> Util::GetFunction(const std::string& s) {
-			return funcs[s];
-		}
+        std::function<void(void)> Util::GetFunction(const std::string& s) {
+        	return funcs[s];
+        }
 
         bool Util::HasFunction(const std::string& s) {
             return funcs.find(s) != funcs.end();
--- src/miniMAT/util/Util.hpp
+++ src/miniMAT/util/Util.hpp
@@ -9,18 +9,18 @@
 #include <Matrix.hpp>
 
 namespace miniMAT {
-	namespace util {
-		class Util {
-		public:
-			static std::map<std::string, std::function<void(void)>> funcs;
+    namespace util {
+        class Util {
+        public:
+            static std::map<std::string, std::function<void(void)>> funcs;
 
-			static void init(std::shared_ptr<std::map<std::string, Matrix>> vars);
+            static void init(std::shared_ptr<std::map<std::string, Matrix>> vars);
 
-			static void PrintResult(std::string varname, Matrix val, bool suppressed);
-			static std::function<void(void)> GetFunction(const std::string& s);
+            static void PrintResult(std::string varname, Matrix val, bool suppressed);
+            static std::function<void(void)> GetFunction(const std::string& s);
             static bool HasFunction(const std::string& s);
-		};
-	}
+        };
+    }
 }
 
 #endif
