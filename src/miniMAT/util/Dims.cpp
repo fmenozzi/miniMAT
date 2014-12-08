996d2f6558452955256895bbc4d8553c9f8f5177
Federico Menozzi <federicogmenozzi@gmail.com>
2014-12-07 20:20:30 -0500

    Add Dims utility

---
+++ src/miniMAT/util/Dims.cpp
@@ -0,0 +1,29 @@
+#include <Dims.hpp>
+
+namespace miniMAT {
+	namespace util {
+		bool Dims::is_scalar(Matrix m) {
+			return m.rows() == 1 && m.cols() == 1;
+		}
+
+		bool Dims::dims_match(Matrix l, Matrix r) {
+			return l.rows() == r.rows() && l.cols() == r.cols();
+		}
+
+		bool Dims::can_multiply(Matrix l, Matrix r) {
+			return l.cols() == r.rows();
+		}
+
+		bool Dims::scalar_mat(Matrix l, Matrix r) {
+			return is_scalar(l) && ! is_scalar(r);
+		}
+
+		bool Dims::mat_scalar(Matrix l, Matrix r) {
+			return ! is_scalar(l) && is_scalar(r);
+		}
+
+		bool Dims::scalar_scalar(Matrix l, Matrix r) {
+			return is_scalar(l) && is_scalar(r);
+		}
+	}
+}
---
+++ src/miniMAT/util/Dims.hpp
@@ -0,0 +1,22 @@
+#ifndef MINIMAT_UTIL_DIMS_HPP
+#define MINIMAT_UTIL_DIMS_HPP
+
+#include <Matrix.hpp>
+
+namespace miniMAT {
+	namespace util {
+		// Convenience functions for determining validity of expressions dimension-wise
+		class Dims {
+		public:
+			static bool is_scalar(Matrix m);
+			static bool dims_match(Matrix l, Matrix r);
+			static bool can_multiply(Matrix l, Matrix r);
+
+			static bool scalar_mat(Matrix l, Matrix r);
+			static bool mat_scalar(Matrix l, Matrix r);
+			static bool scalar_scalar(Matrix l, Matrix r);
+		};
+	}
+}
+
+#endif
