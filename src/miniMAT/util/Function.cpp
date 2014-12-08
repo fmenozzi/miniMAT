64c9085482e13fe9d893ac69f9eab0bdb3750e37
Federico Menozzi <federicogmenozzi@gmail.com>
2014-12-08 15:36:08 -0500

    Add default precision of 4 to numeric output

--- src/miniMAT/util/Function.cpp
+++ src/miniMAT/util/Function.cpp
@@ -44,10 +44,14 @@ namespace miniMAT {
 
         void Function::PrintResult(std::string varname, Matrix m, bool suppressed) {
             if (!suppressed) {
-                // Print a row at a time
+                // Print a row at a time (default precision is 4)
                 std::cout << varname << " =" << std::endl << std::endl;
-                for (int i = 0; i < m.rows(); i++)
-                    std::cout << "    " << m.block(i, 0, 1, m.cols()) << std::endl;
+                for (int i = 0; i < m.rows(); i++) {
+                    std::cout << "    ";
+                    for (int j = 0; j < m.cols(); j++)
+                        std::cout << std::fixed << std::showpoint << std::setprecision(4) << m(i,j) << " ";
+                    std::cout << std::endl;
+                }
                 std::cout << std::endl;
             }
         }
