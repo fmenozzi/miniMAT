0ba790b9b38700b95c48e4bca8b4e3bac22b81ee
Federico Menozzi <federicogmenozzi@gmail.com>
2014-12-15 15:16:26 -0500

    Fix tabbing

--- src/miniMAT/parser/Parser.hpp
+++ src/miniMAT/parser/Parser.hpp
@@ -16,40 +16,39 @@ namespace miniMAT {
     namespace parser {
         class Parser {
         private:
-                lexer::Lexer              lexer;
-                std::deque<lexer::Token>  tokens;
-                bool                      suppressed = false;
+            lexer::Lexer              lexer;
+            std::deque<lexer::Token>  tokens;
+            bool                      suppressed = false;
 
-                std::shared_ptr<reporter::ErrorReporter> reporter;
+            std::shared_ptr<reporter::ErrorReporter> reporter;
 
         public:
-                Parser(const lexer::Lexer& lexer,
-                       std::shared_ptr<reporter::ErrorReporter> reporter);
+            Parser(const lexer::Lexer& lexer, std::shared_ptr<reporter::ErrorReporter> reporter);
 
-                void ParseError(const std::string& error);
+            void ParseError(const std::string& error);
 
-                bool SuppressedOutput();
+            bool SuppressedOutput();
 
-                void Accept(lexer::TokenKind exp_kind);
-                void Accept(lexer::TokenKind exp_kind, const std::string& exp_spelling);
-                void AcceptIt();
+            void Accept(lexer::TokenKind exp_kind);
+            void Accept(lexer::TokenKind exp_kind, const std::string& exp_spelling);
+            void AcceptIt();
 
-                void PutBack(const lexer::Token& t);
+            void PutBack(const lexer::Token& t);
 
-                lexer::Token GetCurrentToken();
+            lexer::Token GetCurrentToken();
 
-                std::shared_ptr<ast::AST> Parse();
+            std::shared_ptr<ast::AST> Parse();
 
-                std::shared_ptr<ast::Statement>  ParseStatement();
+            std::shared_ptr<ast::Statement>  ParseStatement();
 
-                std::shared_ptr<ast::ExprStmt>   ParseExprStmt();
-                std::shared_ptr<ast::AssignStmt> ParseAssignStmt();
+            std::shared_ptr<ast::ExprStmt>   ParseExprStmt();
+            std::shared_ptr<ast::AssignStmt> ParseAssignStmt();
 
-                std::shared_ptr<ast::Expression> ParseExpression();
+            std::shared_ptr<ast::Expression> ParseExpression();
 
-                std::shared_ptr<ast::Expression> ParseA();
-                std::shared_ptr<ast::Expression> ParseB();
-                std::shared_ptr<ast::Expression> ParseC();
+            std::shared_ptr<ast::Expression> ParseA();
+            std::shared_ptr<ast::Expression> ParseB();
+            std::shared_ptr<ast::Expression> ParseC();
         };
     }
 }
