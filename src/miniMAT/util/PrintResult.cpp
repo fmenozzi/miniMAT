b78d86b3911a1321616ad3f756bb217e5162f2ca
Federico Menozzi <federicogmenozzi@gmail.com>
2014-12-26 23:37:33 -0500

    Adjust formatting in util::PrintResult for row vectors

--- src/miniMAT/util/PrintResult.cpp
+++ src/miniMAT/util/PrintResult.cpp
@@ -14,20 +14,27 @@ namespace miniMAT {
                 int precision = 4;
                 int space = 1, dot = 1;
 
-                // Print a row at a time (default precision is 4)
                 cout << varname << " =" << endl << endl;
-                for (int i = 0; i < m.rows(); i++) {
-                    cout << "    ";
+                if (m.rows() == 1) {
+                    // Even spacing between elements in a single row
                     for (int j = 0; j < m.cols(); j++)
-                        cout << right 
-                             << fixed 
-                             << showpoint 
-                             << setprecision(precision) 
-                             << setw(maxdigits + precision + dot + space)
-                             << m(i,j);
+                        cout << right << fixed << showpoint << setprecision(precision) <<  "   " << m(0, j);
+                    cout << endl;
+                } else {
+                    // More complex formatting for multiple rows
+                    for (int i = 0; i < m.rows(); i++) {
+                        cout << "    ";
+                        for (int j = 0; j < m.cols(); j++)
+                            cout << right 
+                                 << fixed 
+                                 << showpoint 
+                                 << setprecision(precision) 
+                                 << setw(maxdigits + precision + dot + space)
+                                 << m(i,j);
+                        cout << endl;
+                    }
                     cout << endl;
                 }
-                cout << endl;
             }
         }
     }
