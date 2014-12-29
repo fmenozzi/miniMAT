d474eb9c1ebce0de01e58d5a7e1741e5093ab204
Federico Menozzi <federicogmenozzi@gmail.com>
2014-12-28 23:05:14 -0500

    Fix spacing

--- src/miniMAT/util/Dims.cpp
+++ src/miniMAT/util/Dims.cpp
@@ -1,33 +1,33 @@
 #include <miniMAT/util/Dims.hpp>
 
 namespace miniMAT {
-	namespace util {
-		bool Dims::is_scalar(ast::Matrix m) {
-			return m.rows() == 1 and m.cols() == 1;
-		}
+    namespace util {
+        bool Dims::is_scalar(ast::Matrix m) {
+            return m.rows() == 1 and m.cols() == 1;
+        }
 
-		bool Dims::dims_match(ast::Matrix l, ast::Matrix r) {
-			return l.rows() == r.rows() and l.cols() == r.cols();
-		}
+        bool Dims::dims_match(ast::Matrix l, ast::Matrix r) {
+            return l.rows() == r.rows() and l.cols() == r.cols();
+        }
 
-		bool Dims::can_multiply(ast::Matrix l, ast::Matrix r) {
-			return l.cols() == r.rows();
-		}
+        bool Dims::can_multiply(ast::Matrix l, ast::Matrix r) {
+            return l.cols() == r.rows();
+        }
 
-		bool Dims::scalar_mat(ast::Matrix l, ast::Matrix r) {
-			return is_scalar(l) and not is_scalar(r);
-		}
+        bool Dims::scalar_mat(ast::Matrix l, ast::Matrix r) {
+            return is_scalar(l) and not is_scalar(r);
+        }
 
-		bool Dims::mat_scalar(ast::Matrix l, ast::Matrix r) {
-			return not is_scalar(l) and is_scalar(r);
-		}
+        bool Dims::mat_scalar(ast::Matrix l, ast::Matrix r) {
+            return not is_scalar(l) and is_scalar(r);
+        }
 
-		bool Dims::scalar_scalar(ast::Matrix l, ast::Matrix r) {
-			return is_scalar(l) and is_scalar(r);
-		}
+        bool Dims::scalar_scalar(ast::Matrix l, ast::Matrix r) {
+            return is_scalar(l) and is_scalar(r);
+        }
 
-		bool Dims::mat_mat(ast::Matrix l, ast::Matrix r) {
-			return not is_scalar(l) and not is_scalar(r);
-		}
-	}
+        bool Dims::mat_mat(ast::Matrix l, ast::Matrix r) {
+            return not is_scalar(l) and not is_scalar(r);
+        }
+    }
 }
--- src/miniMAT/util/Dims.hpp
+++ src/miniMAT/util/Dims.hpp
@@ -4,20 +4,20 @@
 #include <miniMAT/ast/Matrix.hpp>
 
 namespace miniMAT {
-	namespace util {
-		// Convenience functions for determining validity of expressions dimension-wise
-		class Dims {
-		public:
-			static bool is_scalar(ast::Matrix m);
-			static bool dims_match(ast::Matrix l, ast::Matrix r);
-			static bool can_multiply(ast::Matrix l, ast::Matrix r);
+    namespace util {
+        // Convenience functions for determining validity of expressions dimension-wise
+        class Dims {
+        public:
+            static bool is_scalar(ast::Matrix m);
+            static bool dims_match(ast::Matrix l, ast::Matrix r);
+            static bool can_multiply(ast::Matrix l, ast::Matrix r);
 
-			static bool scalar_mat(ast::Matrix l, ast::Matrix r);
-			static bool mat_scalar(ast::Matrix l, ast::Matrix r);
-			static bool scalar_scalar(ast::Matrix l, ast::Matrix r);
-			static bool mat_mat(ast::Matrix l, ast::Matrix r);
-		};
-	}
+            static bool scalar_mat(ast::Matrix l, ast::Matrix r);
+            static bool mat_scalar(ast::Matrix l, ast::Matrix r);
+            static bool scalar_scalar(ast::Matrix l, ast::Matrix r);
+            static bool mat_mat(ast::Matrix l, ast::Matrix r);
+        };
+    }
 }
 
 #endif
