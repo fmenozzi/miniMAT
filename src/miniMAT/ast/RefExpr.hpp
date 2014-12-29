e3893e727850bdc616051633238506fb3a4e674f
Federico Menozzi <federicogmenozzi@gmail.com>
2014-12-29 18:17:04 -0500

    Fix spacing

--- src/miniMAT/ast/RefExpr.hpp
+++ src/miniMAT/ast/RefExpr.hpp
@@ -11,7 +11,7 @@ namespace miniMAT {
         struct RefExpr : public Expression {
             RefExpr(std::shared_ptr<Reference> ref);
 
-            std::string                ClassName() const;
+            std::string ClassName() const;
 
             void VisitDisplay(const std::string& prefix) const;
             ast::Matrix VisitEvaluate(std::shared_ptr<std::map<std::string, ast::Matrix>> vars);
